36bc8be6f60daaf564d6d5f6b7d5569902f9066b
Martin Kuckert <martin@curlybracket.de>
2026-07-03 22:56:08 +0200

    Partially reverts 3fa5696 as long as mcp-commands needs changes

--- .nono/hooks/before.sh
+++ .nono/hooks/before.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-backgrounded.sh start grocery-shopping-app-mcp-androidbuild mcp-commands --dir src --scripts tools/commands --port 12001
+#backgrounded.sh start grocery-shopping-app-mcp-androidbuild mcp-commands --dir src --scripts tools/commands --port 12001
+backgrounded.sh start grocery-shopping-app-mcp-androidbuild uv run --directory tools/mcpandroidbuild mcpandroidbuild --port 12001
 backgrounded.sh start grocery-shopping-app-mcp-websearch docker mcp gateway run --profile web --transport streaming --port 12002
