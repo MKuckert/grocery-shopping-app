a086b047e1f22b6941f2e59e7457963c9b7ce990
Martin Kuckert <martin@curlybracket.de>
2026-07-14 11:41:32 +0200

    feat: implement SyncLifecycleManager for foreground-only PowerSync connection

--- src/app/src/main/kotlin/de/curlybracket/grocery/GroceryApplication.kt
+++ src/app/src/main/kotlin/de/curlybracket/grocery/GroceryApplication.kt
@@ -2,6 +2,16 @@ package de.curlybracket.grocery
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import de.curlybracket.grocery.sync.SyncLifecycleManager
+import javax.inject.Inject
 
 @HiltAndroidApp
-class GroceryApplication : Application()
+class GroceryApplication : Application() {
+
+    @Inject lateinit var syncLifecycleManager: SyncLifecycleManager
+
+    override fun onCreate() {
+        super.onCreate()
+        syncLifecycleManager.initialize()
+    }
+}
